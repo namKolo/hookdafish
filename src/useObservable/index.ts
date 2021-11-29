e3e2011d3342b916e7051567848a0a7454075bec
Nam Tran <pokemon.namkolo@gmail.com>
2021-11-29 10:31:53 +0700

    Add new hook useObservable

--- src/useObservable/index.ts
+++ src/useObservable/index.ts
@@ -1,3 +1,26 @@
-function useObservable() {}
+import { useLayoutEffect, useState } from 'react';
+
+type IValue<T> = T | undefined;
+type UnsubscribeFn = () => void;
+type Listener<T> = (value: T) => void;
+
+export interface Observable<IValue> {
+  subscribe: (
+    listener: Listener<IValue>
+  ) => {
+    unsubscribe: UnsubscribeFn;
+  };
+}
+
+function useObservable<T>(stream$: Observable<IValue<T>>): IValue<T> {
+  const [value, update] = useState<T>();
+
+  useLayoutEffect(() => {
+    const s = stream$.subscribe(update);
+    return () => s.unsubscribe();
+  }, [stream$]);
+
+  return value;
+}
 
 export default useObservable;
