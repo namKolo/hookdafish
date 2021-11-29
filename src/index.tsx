f855bf261fe8a86d5b03c6c1cbbf15f6d568652f
Nam Tran <pokemon.namkolo@gmail.com>
2021-11-29 19:00:38 +0700

    Fix export default module

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,4 @@
-import useEventListener from './useEventListener';
-export default { useEventListener };
+export { default as useEventListener } from './useEventListener';
+export { default as useObservable } from './useObservable';
+export { default as useUnmount } from './useUnmount';
+export { useVirtualList, VirtualList } from './useVirtualList';
